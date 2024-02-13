ef292b952eeb8cb8d684cc6bffdee072c6eba98d
Jose Hillers <josehillers@Saras-MacBook-Air.local>
2024-02-13 14:27:38 +0000

    Task 6: Added loading indicator.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import './App.css'
 import React, { useEffect, useState } from "react";
 import TodoList from "./components/todo/TodoList/TodoList";
-import './App.css'
 import AddItemButton from "./components/UI/AddItemButton";
 export default function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const fetchTasks = async () => {
+    setIsLoading(true);
     const response = await fetch('http://localhost:4000/api/task', {
       headers: {
         'Content-Type': 'Application/json'
       }
     });
     const json = await response.json();
+    setIsLoading(false);
     setTasks(json);
   }
   useEffect(() => {
@@ -23,7 +27,8 @@ export default function App() {
         <AddItemButton/>
       </div>
       <section>
-        <TodoList tasks={tasks} />
+       {!isLoading && <TodoList tasks={tasks} />}
+       {isLoading && <h2>Loading...</h2>}
       </section>
   </>)
 }
